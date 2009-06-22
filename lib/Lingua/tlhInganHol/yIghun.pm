153795a277c1ffb03c98460d8bd3774843fc9ed9
Philip Newton <philip.newton@pobox.com>
2009-06-22 19:47:53 -0400

    Test 0-arg functions, prepare for 1-arg ones.

--- lib/Lingua/tlhInganHol/yIghun.pm
+++ lib/Lingua/tlhInganHol/yIghun.pm
@@ -248,14 +248,34 @@ sub to_change {
 }
 
 my %v_arg0 = qw(
+        poH             time
+        ghomneH         wantarray
+        bogh            fork
+);
+my $v_arg0 = enqr keys %v_arg0;
+
+sub to_arg0 {
+        my ($func) = @_;
+        return $func->{trans};
+}
+
+my %v_arg01 = qw(
         HaD             study
+        'ar             abs
+        joqtaH          sin
+        joqtaHHa'       cos
+        poD             int
+        maHghurtaH      log
+        loS'ar          sqrt
         mIS             rand
+        mIScher         srand
         mach            lc
         wa'DIchmach     lcfirst
         tIn             uc
         wa'DIchtIn      ucfirst
         nargh           quotemeta
         juv             length
+        SIj             split
         toq'a'          defined
         rIn'a'          eof
         mej             exit
@@ -270,19 +290,10 @@ my %v_arg0 = qw(
         mol             dump
         Qong            sleep
         loS             wait
-        ghomneH         wantarray
-        bogh            fork
+        SeppoH          localtime
+        Qo'noSpoH       gmtime
 );
-my $v_arg0 = enqr keys %v_arg0;
-
-my %v_arg0_da = qw(
-        laD             readline
-        chaqpoDmoH      chomp
-        poDmoH          chop
-        chImmoH         undef
-        Say'moH         reset
-);
-my $v_arg0_da = enqr keys %v_arg0_da;
+my $v_arg01 = enqr keys %v_arg01;
 
 my %v_arg1 = qw(
         tlhoch          not
@@ -333,8 +344,6 @@ my %v_arg1 = qw(
         Qong            sleep
         ra'             system
         loS             wait
-        ghomneH         wantarray
-        bogh            fork
 );
 my $v_arg1 = enqr keys %v_arg1;
 sub to_arg1 {
@@ -343,6 +352,15 @@ sub to_arg1 {
         return $arg->{trans}."->$func->{trans}()" if $arg->{object};
         return $func->{trans}."($arg->{trans})";
 }
+
+my %v_arg01_da = qw(
+        laD             readline
+        chaqpoDmoH      chomp
+        poDmoH          chop
+        chImmoH         undef
+        Say'moH         reset
+);
+my $v_arg01_da = enqr keys %v_arg01_da;
 
 my %v_arg1_da = qw(
         poSmoH          open
@@ -360,6 +378,7 @@ my %v_arg1_da = qw(
         baghHa'         untie
 );
 my $v_arg1_da = enqr keys %v_arg1_da;
+
 sub to_arg1_da {
         my ($arg, $func) = @_;
         $arg ||= {trans=>""};   # handle optional args
@@ -767,10 +786,16 @@ sub change {
         pushtok('acc', translate($expr,$pattern,$becomes,$op));
 }
 
+sub arg0 {
+        my ($func) = @_;
+        $func = tok('verb',$func,$v_arg0{$func});
+        pushtok('acc', translate($func));
+}
+
 sub arg1 {
         my ($func) = @_;
         my $arg = top('acc')
-                or $func->{raw} =~ /$v_arg0/
+                or $func =~ /$v_arg01/
                 or die "$func: De' Sambe'!\n" ;        # missing data
         $func = tok('verb',$func,$v_arg1{$func});
         pushtok('acc', translate($arg, $func));
@@ -779,7 +804,7 @@ sub arg1 {
 sub arg1_da {
         my ($func) = @_;
         my $arg = top('dat','abl','dat_handle','abl_handle')
-                or $func =~ /$v_arg0_da/
+                or $func =~ /$v_arg01_da/
                 or die "$func: DoS Hal ghap Sambe'!\n" ;
                                                 # missing target or source
         $func = tok('verb',$func,$v_arg1_da{$func});
@@ -1114,11 +1139,15 @@ FILTER {
                 or /\G$any$v_blockop/gc and blockop("$1")
                 or /\G$sing$v_match/gc  and match("$1")
                 or /\G$any$v_change/gc  and change("$1")
+                or /\G$sing$v_arg0/gc   and arg0("$1")
                 or /\G$sing$v_arg1/gc   and arg1("$1")
-                or /\G$sing$v_arg1_da/gc        and arg1_da("$1")
+                or /\G$sing$v_arg1_da/gc
+                                        and arg1_da("$1")
                 or /\G$plur$v_arg2/gc   and arg2("$1")
-                # or /\G$plur$v_arg2_i/gc       and arg2_i("$1")
-                or /\G$sing$v_arg2_da/gc        and arg2_da("$1")
+                # or /\G$plur$v_arg2_i/gc
+                #                       and arg2_i("$1")
+                or /\G$sing$v_arg2_da/gc
+                                        and arg2_da("$1")
                 or /\G$sing$v_arg2_a/gc and arg2_a("$1")
                 or /\G$any$v_args/gc    and args("$1")
                 or /\G$any$v_args_da/gc and args_da("$1")
@@ -2329,6 +2358,7 @@ The var'aq programming language <http://www.geocities.com/connorbd/varaq/>
         pIn'a'choH      chown
         poD             int
         poDmoH          chop
+        poH             time
         pong            keys
         pongchoH        rename
         pongwI'         caller
@@ -2337,6 +2367,7 @@ The var'aq programming language <http://www.geocities.com/connorbd/varaq/>
         luSpetna'       STDERR (used as name -- i.e. in an open)
         luSpetna'DoS    STDERR (used as handle -- i.e. in a print)
         Qaw'            delete
+        Qo'noSpoH       gmtime
         qoj             or
         qojHa'          atan2
         Qong            sleep
@@ -2350,6 +2381,7 @@ The var'aq programming language <http://www.geocities.com/connorbd/varaq/>
         Sam             index
         SanID           -000
         Say'moH         reset
+        SeppoH          localtime
         SIj             split
         So'             crypt
         Soch            7
@@ -2455,6 +2487,7 @@ The var'aq programming language <http://www.geocities.com/connorbd/varaq/>
         fork            bogh            "be born"
         ge              machbe' law'    "be not smaller"
         glob            Sach            "expand"
+        gmtime          Qo'noS poH      "Kronos time"
         goto            jaH             "go"
         grep            wIv             "choose"
         gt              tIn law'        "be larger"
@@ -2478,6 +2511,7 @@ The var'aq programming language <http://www.geocities.com/connorbd/varaq/>
         length          juv             "measure"
         link            rar             "connect"
         local           vam             "this"
+        localtime       SeppoH          "region's time"
         log             maHghurtaH      "ten log"
         lt              mach law'       "be smaller"
         m               ghov            "recognize"
@@ -2535,6 +2569,7 @@ The var'aq programming language <http://www.geocities.com/connorbd/varaq/>
         tell            ja'             "report"
         tie             bagh            "tie"
         tied            bagh'a'         "is tied"
+        time            poH             "time"
         tr              mugh            "translate"
         truncate        nup             "decrease"
         uc              tIn             "be big"
--- t/06.mock.t
+++ t/06.mock.t
@@ -2,7 +2,7 @@
 # vim:set et si:
 #
 use Test::More
-        tests => 858
+        tests => 903
 ;
 use Carp;
 use Data::Dumper;
@@ -73,6 +73,12 @@ wrap $_ for qw(
         match
         to_change
         change
+        to_arg0
+        to_arg1
+        to_arg1_da
+        arg0
+        arg1
+        arg1_da
 );
 
 =for comment
@@ -82,8 +88,6 @@ wrap $_ for qw(
         readline_honourably
         top
         to_go
-        to_arg1
-        to_arg1_da
         to_arg2
         to_arg2_da
         to_arg2_a
@@ -99,8 +103,6 @@ wrap $_ for qw(
         to_ternop
         to_control
         go
-        arg1
-        arg1_da
         arg2
         arg2_da
         arg2_a
@@ -1231,6 +1233,66 @@ sub {
         is $stack[6]{result}[0]{raw}, 'varvaD << abc >> << def >> mugh', 'token->raw';
         is $stack[6]{result}[0]{trans}, '$var =~ tr< abc >< def >', 'token->trans';
 },
+sub {
+        note 'time';
+        my $step = shift;
+        is $step, 39, 'step 39';
+        my @stack = extract_stack($step);
+        is scalar(@stack), 4, '4 entries on callstack';
+        is $stack[0]{name}, 'to_arg0', 'to_arg0';
+        is $stack[0]{result}[0], 'time', 'to_arg0() -> time';
+        is $stack[1]{name}, 'translate', 'translate';
+        is $stack[1]{result}[0], 'poH', 'translate->raw';
+        is $stack[1]{result}[1], 'time', 'translate->trans';
+        is $stack[2]{name}, 'pushtok', 'Push token acc';
+        is $stack[2]{result}[0]{type}, 'acc', 'token->type';
+        is $stack[2]{result}[0]{raw}, 'poH', 'token->raw';
+        is $stack[2]{result}[0]{trans}, 'time', 'token->trans';
+        is $stack[3]{name}, 'arg0', 'arg0';
+        is $stack[3]{result}[0]{type}, 'acc', 'token->type';
+        is $stack[3]{result}[0]{raw}, 'poH', 'token->raw';
+        is $stack[3]{result}[0]{trans}, 'time', 'token->trans';
+},
+sub {
+        note 'wantarray';
+        my $step = shift;
+        is $step, 40, 'step 40';
+        my @stack = extract_stack($step);
+        is scalar(@stack), 4, '4 entries on callstack';
+        is $stack[0]{name}, 'to_arg0', 'to_arg0';
+        is $stack[0]{result}[0], 'wantarray', 'to_arg0() -> wantarray';
+        is $stack[1]{name}, 'translate', 'translate';
+        is $stack[1]{result}[0], 'ghomneH', 'translate->raw';
+        is $stack[1]{result}[1], 'wantarray', 'translate->trans';
+        is $stack[2]{name}, 'pushtok', 'Push token acc';
+        is $stack[2]{result}[0]{type}, 'acc', 'token->type';
+        is $stack[2]{result}[0]{raw}, 'ghomneH', 'token->raw';
+        is $stack[2]{result}[0]{trans}, 'wantarray', 'token->trans';
+        is $stack[3]{name}, 'arg0', 'arg0';
+        is $stack[3]{result}[0]{type}, 'acc', 'token->type';
+        is $stack[3]{result}[0]{raw}, 'ghomneH', 'token->raw';
+        is $stack[3]{result}[0]{trans}, 'wantarray', 'token->trans';
+},
+sub {
+        note 'fork';
+        my $step = shift;
+        is $step, 41, 'step 41';
+        my @stack = extract_stack($step);
+        is scalar(@stack), 7, '7 entries on callstack';
+        is $stack[1]{name}, 'to_arg0', 'to_arg0';
+        is $stack[1]{result}[0], 'fork', 'to_arg0() -> fork';
+        is $stack[2]{name}, 'translate', 'translate';
+        is $stack[2]{result}[0], 'bogh', 'translate->raw';
+        is $stack[2]{result}[1], 'fork', 'translate->trans';
+        is $stack[3]{name}, 'pushtok', 'Push token acc';
+        is $stack[3]{result}[0]{type}, 'acc', 'token->type';
+        is $stack[3]{result}[0]{raw}, 'bogh', 'token->raw';
+        is $stack[3]{result}[0]{trans}, 'fork', 'token->trans';
+        is $stack[4]{name}, 'arg0', 'arg0';
+        is $stack[4]{result}[0]{type}, 'acc', 'token->type';
+        is $stack[4]{result}[0]{raw}, 'bogh', 'token->raw';
+        is $stack[4]{result}[0]{trans}, 'fork', 'token->trans';
+},
 ];
 
 my @module_args;
@@ -1477,5 +1539,27 @@ varvaD << abc >> << def >> tImugh!
 wejmaH chorgh yIvan!
 nabvaD 'olvo' wejmaH chorgh DIch yInob!
 wejmaH chorgh yInabvetlh!
+
+wejmaH Hut yIlIH!
+yIpoH!
+wejmaH Hut yIvan!
+nabvaD 'olvo' wejmaH Hut DIch yInob!
+wejmaH Hut yInabvetlh!
+
+loSmaH yIlIH!
+yIghomneH!
+loSmaH yIvan!
+nabvaD 'olvo' loSmaH DIch yInob!
+loSmaH yInabvetlh!
+
+# fork: exit if we're the child
+# my $pid; $pid = fork; if ($pid == 0) { exit; }
+pIDwIj!
+loSmaH wa' yIlIH! #'
+pIDvaD yIbogh yInob!
+loSmaH wa' yIvan! #'
+{ yImej! } pID pagh mI'rap'a' teHchugh! #'
+nabvaD 'olvo' loSmaH wa' DIch yInob! #'
+loSmaH wa' yInabvetlh! #'
 
 yIdone_testing!
